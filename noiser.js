fc6c0f04ed00efdca96fee83d75d562a853d6cc0
Ana Bastos <ana.bastos881@gmail.com>
2016-11-28 01:29:20 -0200

    Ajustando ondinhas

--- noiser.js
+++ noiser.js
@@ -1,15 +1,15 @@
-let Noise = function() {
+let Noise = () => {
     const MAX_VERTICES = 256
     const MAX_VERTICES_MASK = MAX_VERTICES -1
 
     let r = Array.from({length: MAX_VERTICES}, () => (Math.random()))
 
-    let lerp = function(a, b, t ) {
+    let lerp = (a, b, t ) => {
         return a * ( 1 - t ) + b * t
     }
 
     return {
-        getVal: function(x){
+        getVal: (x) => {
             let xFloor = Math.floor(x)
             let t = x - xFloor
             let step = t * t * ( 3 - 2 * t )
@@ -23,27 +23,31 @@ let Noise = function() {
     }
 }
 
-let Ondinha = function(){
+let Ondinha = () => {
     const canvas = document.querySelector("canvas");
     const context = canvas.getContext("2d");
     const noiseScale = 0.02
-    const canvasP = canvas.width/3
-    const waves = Array.from({length: canvas.width}, () => (Noise().getVal(noiseScale)))
-    context.beginPath()
-    context.lineWidth = "0.1"
+    const canvasP = canvas.width/4
 
-    let multiplier = 1
-    waves.map((wave, index) => {
-        let start = 0
-        
-        context.moveTo(index, 10 + wave * multiplier - 0.5)
-        context.lineTo(index, 10 + wave * multiplier)
-        context.strokeStyle = "white"
-        context.stroke()
-        console.log(index)
-        multiplier = index > canvasP && index < canvasP * 2? -6 : 1
-    })
+    return {
+        drawWave: (y) => {
+            const waves = Array.from({length: canvas.width}, () => (Noise().getVal(noiseScale)))
+            context.beginPath()
+            context.lineWidth = "0.3"
+            let waveX = 0
+            waves.map((wave, index) => {
+                multiplier = index > canvasP && index < canvasP * 3 ? -30 : 1
+                context.moveTo(index, y + waveX * multiplier)
+                context.lineTo(index, y + wave * multiplier)
+                context.strokeStyle = "white"
+                context.stroke()
+                waveX = wave
+            })
+        }
+    }
+
 }
 
-
-Ondinha();
+Ondinha().drawWave(30);
+Ondinha().drawWave(60);
+Ondinha().drawWave(90);
